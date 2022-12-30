bd0ccb719a74bc4adb73d1bab0d215468560c500
ma-jonzz <mohamed@hello-pomelo.com>
2022-12-30 18:03:10 +0100

    Update composer config

--- ValidatorBundle/DependencyInjection/Configuration.php
+++ ValidatorBundle/DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('validator');
+        $treeBuilder = new TreeBuilder('validator');
         return ($treeBuilder);
     }
 }
